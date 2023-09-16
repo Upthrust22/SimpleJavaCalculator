27d6b15513f634fef329f56d76044e708839dbd9
Upthrust22 <105308552+Upthrust22@users.noreply.github.com>
2023-09-16 09:21:59 -0700

    A simple Calculator in Java using basic arithmetic operator

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,24 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    static void calculator(double a, String operator, double b) {
-        if(operator.equals("+")) {
-            System.out.println(a + b);
-        } else if (operator.equals("-")) {
-            System.out.println(a - b);
-        } else if (operator.equals("*")) {
-            System.out.println(a * b);
-        } else if (operator.equals("/")) {
-            System.out.println(a / b);
-        } else if (operator.equals("%")) {
-            System.out.println(a % b);
-        } else {
-            System.out.println("enter correct operator");
+    static double calculator(double a, char operator, double b) {
+        if(operator == '+') {
+            return a + b;
+        } else if (operator == '-') {
+            return a - b;
+        } else if (operator == '*') {
+            return a * b;
+        } else if (operator == '/') {
+            return a / b;
+        } else if (operator == '%') {
+            return a % b;
         }
     }
     public static void main(String[] args) {
-        calculator(5, "+", 6);
-        calculator(10, "-", 6);
-        calculator(120, "*", 6);
-        calculator(300, "/", 6);
-        calculator(525, "%", 6);
+        System.out.println(calculator(5, '+', 6));
+        System.out.println(calculator(10, '-', 3));
+        System.out.println(calculator(120, '*', 7));
+        System.out.println(calculator(300, '/', 15));
+        System.out.println(calculator(525, '%', 98));
     }
 }
